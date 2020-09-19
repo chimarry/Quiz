ba3c16622da778301e8472177d81c9a713c24e23
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2020-09-19 19:30:39 +0200

    Add factory for resolving dependencies

---
+++ AqoonQuiz/AqoonQuiz/DependencyFactory.cs
@@ -0,0 +1,25 @@
+﻿using AqoonQuiz.ErrorHandling;
+using Serilog;
+using System;
+using System.IO;
+
+namespace AqoonQuiz
+{
+    /// <summary>
+    /// A factory responsible for resolving interface dependencies.
+    /// </summary>
+    public static class DependencyFactory
+    {
+        /// <summary>
+        /// Creates logger.
+        /// </summary>
+        /// <returns>Configured IAqoonLogger object</returns>
+        public static IAqoonLogger GetLogger()
+        {
+            LoggerConfiguration logConfig = new LoggerConfiguration();
+            return new SerilogAqoonLogger(logConfig.MinimumLevel.Information()
+                                            .WriteTo.File($"log-{DateTime.Now}.txt")
+                                            .CreateLogger());
+        }
+    }
+}
